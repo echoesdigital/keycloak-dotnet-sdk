56a8f573b69caeb6d16c8ff5856c99038efc55a5
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-02-27 14:39:49 -0300

    feat: implement client resource

---
+++ src/Keycloak.Dotnet.Sdk/Dtos/Clients/ClientDto.cs
@@ -0,0 +1,37 @@
+﻿namespace Echoes.Digital.Keycloak.Dotnet.Sdk.Dtos.Clients;
+
+public record ClientDto
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("clientId")]
+    public string? ClientId { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("rootUrl")]
+    public string? RootUrl { get; set; }
+
+    [JsonPropertyName("adminUrl")]
+    public string? AdminUrl { get; set; }
+
+    [JsonPropertyName("baseUrl")]
+    public string? BaseUrl { get; set; }
+
+    [JsonPropertyName("redirectUris")]
+    public List<string>? RedirectUris { get; set; }
+
+    [JsonPropertyName("webOrigins")]
+    public List<string>? WebOrigins { get; set; }
+
+    [JsonPropertyName("enabled")]
+    public bool? Enabled { get; set; }
+
+    [JsonPropertyName("attributes")]
+    public Dictionary<string, string>? Attributes { get; set; }
+}
