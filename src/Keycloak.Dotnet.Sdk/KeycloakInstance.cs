02e8ddc05fc1eca60239b635d0d295227f4b6f39
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-02-27 16:20:37 -0300

    feat: implement role resource

--- src/Keycloak.Dotnet.Sdk/KeycloakInstance.cs
+++ src/Keycloak.Dotnet.Sdk/KeycloakInstance.cs
@@ -10,11 +10,17 @@ public class KeycloakInstance : IDisposable
         Realms = new RealmsResource(this);
         Clients = new ClientsResource(this);
         Users = new UsersResource(this);
+        Roles = new RolesResource(this);
+        UserRoleMappings = new UserRoleMappingsResource(this);
+        ClientScopes = new ClientScopesResource(this);
     }
 
     public RealmsResource Realms { get; }
     public ClientsResource Clients { get; }
     public UsersResource Users { get; }
+    public RolesResource Roles { get; }
+    public UserRoleMappingsResource UserRoleMappings { get; }
+    public ClientScopesResource ClientScopes { get; }
 
     public void Dispose()
     {
