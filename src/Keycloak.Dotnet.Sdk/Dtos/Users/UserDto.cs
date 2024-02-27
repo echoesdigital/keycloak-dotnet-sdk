0584a5012bc5b01d9fa0f2b73cf6a226798e6b99
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-02-27 15:40:11 -0300

    feat: implement user resource

---
+++ src/Keycloak.Dotnet.Sdk/Dtos/Users/UserDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Echoes.Digital.Keycloak.Dotnet.Sdk.Dtos.Users;
+
+public record UserDto
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("username")]
+    public string? Username { get; set; }
+
+    [JsonPropertyName("enabled")]
+    public bool? Enabled { get; set; }
+
+    [JsonPropertyName("firstName")]
+    public string? FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string? LastName { get; set; }
+
+    [JsonPropertyName("email")]
+    public string? Email { get; set; }
+
+    [JsonPropertyName("emailVerified")]
+    public bool? EmailVerified { get; set; }
+
+    [JsonPropertyName("attributes")]
+    public Dictionary<string, List<string>>? Attributes { get; set; }
+}
---
+++ src/Keycloak.Dotnet.Sdk/Resources/UsersResource.cs
@@ -0,0 +1,103 @@
+﻿namespace Echoes.Digital.Keycloak.Dotnet.Sdk.Resources;
+
+public class UsersResource(KeycloakInstance instance)
+{
+    public async Task<ResultResponseDto> Create(string realm, UserDto user)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users", UriKind.Relative);
+
+        using var response = await instance.HttpClient.PostJsonAsync(uri, user).ConfigureAwait(false);
+
+        return new ResultResponseDto
+        {
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = await response.Content.ReadAsStringAsync().ConfigureAwait(false)
+            }
+        };
+    }
+
+    public async Task<ResultResponseDto<UserDto>> Get(string realm, string userId)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users/{userId}", UriKind.Relative);
+
+        using var response = await instance.HttpClient.GetAsync(uri).ConfigureAwait(false);
+
+        return new ResultResponseDto<UserDto>
+        {
+            Data = await response.Content.ReadAsJsonAsync<UserDto>().ConfigureAwait(false),
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = !response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : null
+            }
+        };
+    }
+
+    public async Task<ResultResponseDto<List<UserDto>>> List(string realm, UserFilterDto filter)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users{filter.ToQueryString()}", UriKind.Relative);
+
+        using var response = await instance.HttpClient.GetAsync(uri).ConfigureAwait(false);
+
+        return new ResultResponseDto<List<UserDto>>
+        {
+            Data = await response.Content.ReadAsJsonAsync<List<UserDto>>().ConfigureAwait(false),
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = !response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : null
+            }
+        };
+    }
+
+    public async Task<ResultResponseDto<int>> Count(string realm, UserFilterDto filter)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users/count{filter.ToQueryString()}", UriKind.Relative);
+
+        using var response = await instance.HttpClient.GetAsync(uri).ConfigureAwait(false);
+
+        return new ResultResponseDto<int>
+        {
+            Data = await response.Content.ReadAsJsonAsync<int>().ConfigureAwait(false),
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = !response.IsSuccessStatusCode ? await response.Content.ReadAsStringAsync().ConfigureAwait(false) : null
+            }
+        };
+    }
+
+    public async Task<ResultResponseDto> Update(string realm, UserDto user)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users/{user.Id}", UriKind.Relative);
+
+        using var response = await instance.HttpClient.PutJsonAsync(uri, user).ConfigureAwait(false);
+
+        return new ResultResponseDto
+        {
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = await response.Content.ReadAsStringAsync().ConfigureAwait(false)
+            }
+        };
+    }
+
+    public async Task<ResultResponseDto> Delete(string realm, string id)
+    {
+        var uri = new Uri($"admin/realms/{realm}/users/{id}", UriKind.Relative);
+
+        using var response = await instance.HttpClient.DeleteAsync(uri).ConfigureAwait(false);
+
+        return new ResultResponseDto
+        {
+            Result =
+            {
+                Success = response.IsSuccessStatusCode,
+                Message = await response.Content.ReadAsStringAsync().ConfigureAwait(false)
+            }
+        };
+    }
+}
