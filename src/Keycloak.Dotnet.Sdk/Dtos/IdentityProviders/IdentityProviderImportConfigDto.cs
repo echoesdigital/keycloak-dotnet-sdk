02c0bcd71ef18580de9035684899e177891f03d4
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-02-29 10:16:08 -0300

    feat: add import config into identity provider resource

---
+++ src/Keycloak.Dotnet.Sdk/Dtos/IdentityProviders/IdentityProviderImportConfigDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Echoes.Digital.Keycloak.Dotnet.Sdk.Dtos.IdentityProviders;
+
+public record IdentityProviderImportConfigDto
+{
+    [JsonPropertyName("fromUrl")]
+    public string? FromUrl { get; init; }
+
+    [JsonPropertyName("providerId")]
+    public string? ProviderId { get; init; }
+}
