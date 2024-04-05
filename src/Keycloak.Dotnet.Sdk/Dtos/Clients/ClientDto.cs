c4c4ceb02029d01bc9ce74ca7458aae6a4279294
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-04-05 13:34:48 -0300

    feat: add new properties "publicClient" and "directAccessGrantsEnabled"

--- src/Keycloak.Dotnet.Sdk/Dtos/Clients/ClientDto.cs
+++ src/Keycloak.Dotnet.Sdk/Dtos/Clients/ClientDto.cs
@@ -2,36 +2,42 @@
 
 public record ClientDto
 {
-    [JsonPropertyName("id")]
-    public string? Id { get; set; }
-
-    [JsonPropertyName("clientId")]
-    public string? ClientId { get; set; }
-
-    [JsonPropertyName("name")]
-    public string? Name { get; set; }
-
-    [JsonPropertyName("description")]
-    public string? Description { get; set; }
-
-    [JsonPropertyName("rootUrl")]
-    public string? RootUrl { get; set; }
-
     [JsonPropertyName("adminUrl")]
     public string? AdminUrl { get; set; }
+
+    [JsonPropertyName("attributes")]
+    public Dictionary<string, string>? Attributes { get; set; }
 
     [JsonPropertyName("baseUrl")]
     public string? BaseUrl { get; set; }
 
-    [JsonPropertyName("redirectUris")]
-    public List<string>? RedirectUris { get; set; }
+    [JsonPropertyName("clientId")]
+    public string? ClientId { get; set; }
 
-    [JsonPropertyName("webOrigins")]
-    public List<string>? WebOrigins { get; set; }
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+
+    [JsonPropertyName("directAccessGrantsEnabled")]
+    public bool? DirectAccessGrantsEnabled { get; set; }
 
     [JsonPropertyName("enabled")]
     public bool? Enabled { get; set; }
 
-    [JsonPropertyName("attributes")]
-    public Dictionary<string, string>? Attributes { get; set; }
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+
+    [JsonPropertyName("publicClient")]
+    public bool? PublicClient { get; set; }
+
+    [JsonPropertyName("redirectUris")]
+    public List<string>? RedirectUris { get; set; }
+
+    [JsonPropertyName("rootUrl")]
+    public string? RootUrl { get; set; }
+
+    [JsonPropertyName("webOrigins")]
+    public List<string>? WebOrigins { get; set; }
 }
