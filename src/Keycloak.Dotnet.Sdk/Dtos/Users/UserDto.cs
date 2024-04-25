90721568a04a55af935a22e4fed7bcb9f8fa00f5
Vinicius Oliveira Gouveia <42838209+Matrix2100@users.noreply.github.com>
2024-04-25 20:35:52 -0300

    feat: add user credentials into DTO

--- src/Keycloak.Dotnet.Sdk/Dtos/Users/UserDto.cs
+++ src/Keycloak.Dotnet.Sdk/Dtos/Users/UserDto.cs
@@ -25,4 +25,64 @@ public record UserDto
 
     [JsonPropertyName("attributes")]
     public Dictionary<string, List<string>>? Attributes { get; set; }
+
+    [JsonPropertyName("credentials")]
+    public List<CredentialsDto>? Credentials { get; set; }
+
+    public record CredentialsDto
+    {
+        [JsonPropertyName("id")]
+        public string? Id { get; init; }
+
+        [JsonPropertyName("type")]
+        public string? Type { get; init; }
+
+        [JsonPropertyName("userLabel")]
+        public string? UserLabel { get; init; }
+
+        [JsonPropertyName("createdDate")]
+        public long? CreatedDate { get; init; }
+
+        [JsonPropertyName("secretData")]
+        public string? SecretData { get; init; }
+
+        [JsonPropertyName("credentialData")]
+        public string? CredentialData { get; init; }
+
+        [JsonPropertyName("priority")]
+        public int? Priority { get; init; }
+
+        [JsonPropertyName("value")]
+        public string? Value { get; init; }
+
+        [JsonPropertyName("temporary")]
+        public bool? Temporary { get; init; }
+
+        [JsonPropertyName("device")]
+        public string? Device { get; init; }
+
+        [JsonPropertyName("hashedSaltedValue")]
+        public string? HashedSaltedValue { get; init; }
+
+        [JsonPropertyName("salt")]
+        public string? Salt { get; init; }
+
+        [JsonPropertyName("hashIterations")]
+        public int? HashIterations { get; init; }
+
+        [JsonPropertyName("counter")]
+        public int? Counter { get; init; }
+
+        [JsonPropertyName("algorithm")]
+        public string? Algorithm { get; init; }
+
+        [JsonPropertyName("digits")]
+        public int? Digits { get; init; }
+
+        [JsonPropertyName("period")]
+        public int? Period { get; init; }
+
+        [JsonPropertyName("config")]
+        public Dictionary<string, object>? Config { get; init; }
+    }
 }
